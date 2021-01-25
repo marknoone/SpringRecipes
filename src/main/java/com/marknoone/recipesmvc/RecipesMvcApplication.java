ecb795518fae11f53e494256b0fb01d1e2a54594
Mark Noone <marknoone@Marks-MacBook-Pro.local>
2021-01-25 10:48:29 +0000

    Init commit

---
+++ src/main/java/com/marknoone/recipesmvc/RecipesMvcApplication.java
@@ -0,0 +1,13 @@
+package com.marknoone.recipesmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marknoone/recipesmvc/RecipesMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marknoone.recipesmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
